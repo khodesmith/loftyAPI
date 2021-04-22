a3c614067ee4a5bc8f38b6f5a7841503060048b2
Azeez Ojeyinka <azeezojeyinka@yahoo.com>
2021-04-22 11:18:19 +0100

    first API commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const port = process.env.port || 5454;
+const cors = require('cors');
+// const view = require('./Component/View')
+const app = express();
+// const OFFLINE_DB = "mongodb://localhost/money";
+const ONLINE_DB = 
+"mongodb+srv://AzeezLSETF:sunlarry...@cluster0.in8ws.mongodb.net/money?retryWrites=true&w=majority"
+
+mongoose.connect(ONLINE_DB,{
+    useCreateIndex: true,
+    useFindAndModify:false,
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log(`Database is active`)
+})
+app.use(express.json());
+app.use("/", require("./Component/Controller"))
+app.use(cors())
+
+
+app.listen(port,()=>{
+    console.log(`port is listening`);
+})
