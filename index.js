a897b2ea405c935be0e8513a1d9a45298c7986f1
Azeez Ojeyinka <azeezojeyinka@yahoo.com>
2021-04-24 12:51:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const port = process.env.port || 5454;
+const port = process.env.PORT || 5454;
 const cors = require('cors');
 const view = require('./Component/Controller')
 const app = express();
